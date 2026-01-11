f549d1752c61a8e08df1a90e8f2a9ee4014c3be8
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:33:34 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "key_vault_access_policys" {
+  description = "All key_vault_access_policy resources"
+  value       = azurerm_key_vault_access_policy.key_vault_access_policys
+}
+
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "key_vault_access_policys" {
+  description = <<EOT
+Map of key_vault_access_policys, attributes below
+Required:
+    - key_vault_id
+    - object_id
+    - tenant_id
+Optional:
+    - application_id
+    - certificate_permissions
+    - key_permissions
+    - secret_permissions
+    - storage_permissions
+EOT
+
+  type = map(object({
+    key_vault_id            = string
+    object_id               = string
+    tenant_id               = string
+    application_id          = optional(string)
+    certificate_permissions = optional(list(string))
+    key_permissions         = optional(list(string))
+    secret_permissions      = optional(list(string))
+    storage_permissions     = optional(list(string))
+  }))
+}
+
