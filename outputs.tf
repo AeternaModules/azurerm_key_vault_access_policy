3ff626134cc11a628a8fd9cacf40cae01ddfeb3f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:59 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "key_vault_access_policies" {
-  description = "All key_vault_access_policy resources"
-  value       = azurerm_key_vault_access_policy.key_vault_access_policies
-}
 output "key_vault_access_policies_application_id" {
-  description = "List of application_id values across all key_vault_access_policies"
-  value       = [for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : v.application_id]
+  description = "Map of application_id values across all key_vault_access_policies, keyed the same as var.key_vault_access_policies"
+  value       = { for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : k => v.application_id }
 }
 output "key_vault_access_policies_certificate_permissions" {
-  description = "List of certificate_permissions values across all key_vault_access_policies"
-  value       = [for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : v.certificate_permissions]
+  description = "Map of certificate_permissions values across all key_vault_access_policies, keyed the same as var.key_vault_access_policies"
+  value       = { for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : k => v.certificate_permissions }
 }
 output "key_vault_access_policies_key_permissions" {
-  description = "List of key_permissions values across all key_vault_access_policies"
-  value       = [for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : v.key_permissions]
+  description = "Map of key_permissions values across all key_vault_access_policies, keyed the same as var.key_vault_access_policies"
+  value       = { for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : k => v.key_permissions }
 }
 output "key_vault_access_policies_key_vault_id" {
-  description = "List of key_vault_id values across all key_vault_access_policies"
-  value       = [for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : v.key_vault_id]
+  description = "Map of key_vault_id values across all key_vault_access_policies, keyed the same as var.key_vault_access_policies"
+  value       = { for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : k => v.key_vault_id }
 }
 output "key_vault_access_policies_object_id" {
-  description = "List of object_id values across all key_vault_access_policies"
-  value       = [for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : v.object_id]
+  description = "Map of object_id values across all key_vault_access_policies, keyed the same as var.key_vault_access_policies"
+  value       = { for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : k => v.object_id }
 }
 output "key_vault_access_policies_secret_permissions" {
-  description = "List of secret_permissions values across all key_vault_access_policies"
-  value       = [for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : v.secret_permissions]
+  description = "Map of secret_permissions values across all key_vault_access_policies, keyed the same as var.key_vault_access_policies"
+  value       = { for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : k => v.secret_permissions }
 }
 output "key_vault_access_policies_storage_permissions" {
-  description = "List of storage_permissions values across all key_vault_access_policies"
-  value       = [for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : v.storage_permissions]
+  description = "Map of storage_permissions values across all key_vault_access_policies, keyed the same as var.key_vault_access_policies"
+  value       = { for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : k => v.storage_permissions }
 }
 output "key_vault_access_policies_tenant_id" {
-  description = "List of tenant_id values across all key_vault_access_policies"
-  value       = [for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : v.tenant_id]
+  description = "Map of tenant_id values across all key_vault_access_policies, keyed the same as var.key_vault_access_policies"
+  value       = { for k, v in azurerm_key_vault_access_policy.key_vault_access_policies : k => v.tenant_id }
 }
 
